039669d3518e3a88d9c2995542887c0077a264dc
thebigmunch <mail@thebigmunch.me>
2016-01-11 14:37:05 -0500

    Update copyright year

--- gmusicapi_wrapper/__init__.py
+++ gmusicapi_wrapper/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, unicode_literals
 __title__ = 'gmusicapi_wrapper'
 __version__ = "0.1.0"
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2015 thebigmunch <mail@thebigmunch.me>'
+__copyright__ = 'Copyright 2016 thebigmunch <mail@thebigmunch.me>'
 
 import logging
 
