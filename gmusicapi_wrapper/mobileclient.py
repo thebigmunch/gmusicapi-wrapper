5a3cc4572958444a935113223518fddeff19556d
thebigmunch <mail@thebigmunch.me>
2016-02-27 20:24:39 -0500

    Add get_google_playlist method to MobileClientWrapper

--- gmusicapi_wrapper/mobileclient.py
+++ gmusicapi_wrapper/mobileclient.py
@@ -102,3 +102,22 @@ class MobileClientWrapper(_Base):
 		logger.info("Loaded {0} Google Music songs".format(len(matched_songs)))
 
 		return matched_songs, filtered_songs
+
+	def get_google_playlist(self, playlist):
+		"""Get playlist information of a user-generated Google Music playlist.
+
+		Returns the playlist dict as returned by Mobileclient.get_all_user_playlist_contents.
+
+		:param playlist: Name or ID of Google Music playlist. Names are case-sensitive.
+		  Google allows multiple playlists with the same name.
+		  If multiple playlists have the same name, the first one encountered is used.
+		"""
+
+		logger.info("Loading playlist {0}".format(playlist))
+
+		for google_playlist in self.api.get_all_user_playlist_contents():
+			if google_playlist['name'] == playlist or google_playlist['id'] == playlist:
+				return google_playlist
+		else:
+			logger.warning("Playlist {0} does not exist.".format(playlist))
+			return {}
