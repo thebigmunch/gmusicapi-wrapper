6bbddc5b2877ce321a4265b2edd7368e1b2c95a6
thebigmunch <mail@thebigmunch.me>
2016-02-27 20:24:48 -0500

    Add get_google_playlist_songs method to MobileClientWrapper

--- gmusicapi_wrapper/mobileclient.py
+++ gmusicapi_wrapper/mobileclient.py
@@ -121,3 +121,49 @@ class MobileClientWrapper(_Base):
 		else:
 			logger.warning("Playlist {0} does not exist.".format(playlist))
 			return {}
+
+	def get_google_playlist_songs(self, playlist, include_filters=None, exclude_filters=None, all_includes=False, all_excludes=False):
+		"""Create song list from a user-generated Google Music playlist.
+
+		Returns, from given playlist, a list of Google Music song dicts matching criteria and
+		a list of Google Music song dicts filtered out using filter criteria.
+
+		:param playlist: Name or ID of Google Music playlist. Names are case-sensitive.
+		  Google allows multiple playlists with the same name.
+		  If multiple playlists have the same name, the first one encountered is used.
+
+		:param include_filters: A list of ``(field, pattern)`` tuples.
+		  Fields are any valid Google Music metadata field available to the Musicmanager client.
+		  Patterns are Python regex patterns.
+
+		  Google Music songs are filtered out if the given metadata field values don't match any of the given patterns.
+
+		:param exclude_filters: A list of ``(field, pattern)`` tuples.
+		  Fields are any valid Google Music metadata field available to the Musicmanager client.
+		  Patterns are Python regex patterns.
+
+		  Google Music songs are filtered out if the given metadata field values match any of the given patterns.
+
+		:param all_includes: If ``True``, all include_filters criteria must match to include a song.
+
+		:param all_excludes: If ``True``, all exclude_filters criteria must match to exclude a song.
+		"""
+
+		logger.info("Loading Google Music playlist songs...")
+
+		google_playlist = self.get_google_playlist(playlist)
+
+		if not google_playlist:
+			return [], []
+
+		playlist_song_ids = [track['trackId'] for track in google_playlist['tracks']]
+		playlist_songs = [song for song in self.api.get_all_songs() if song['id'] in playlist_song_ids]
+
+		matched_songs, filtered_songs = filter_google_songs(
+			playlist_songs, include_filters, exclude_filters, all_includes, all_excludes
+		)
+
+		logger.info("Filtered {0} Google playlist songs".format(len(filtered_songs)))
+		logger.info("Loaded {0} Google playlist songs".format(len(matched_songs)))
+
+		return matched_songs, filtered_songs
